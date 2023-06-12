185dcd5ec2722f876b16b12f64a743a6785d92f1
Emmz900 <68696398+Emmz900@users.noreply.github.com>
2023-06-12 15:12:36 +0100

    creation of candy project, tidying birds

--- task_3/data_cleaning_scripts/cleaning.R
+++ task_3/data_cleaning_scripts/cleaning.R
@@ -52,26 +52,38 @@ seabirds_joined %>%
 # There is one record missing the common name and all other species information,
 # this record is therefore not useful to us and can be removed.
 # There are also records which state no birds were recorded, these will be removed.
+  
 seabirds_joined <- seabirds_joined %>% 
+  # Remove missing species or no birds records
   filter(!is.na(species_scientific_name),
          species_common_name != "[NO BIRDS RECORDED]") %>% 
-  mutate(species_common_name = case_match(species_common_name, 
-             "White capped albatross" ~
-               "Shy / white-capped / Salvin's / Chatham mollymawk",
-             .default = species_common_name)) %>% 
-  # seperate species family and genus
-  mutate(species_common_name = 
-           # str_extract(species_common_name,
-           #             "[A-Z]+[a-z\\(\\) /'-]+[A-Z]*[a-z\\(\\) /'-]*"))
-           str_remove(species_common_name, "[:upper:]*[0-9]*$"),
-         species_abbreviation = 
-           str_extract(species_abbreviation, "^[:upper:]+"),
-         family =
-           str_extract(species_scientific_name, "^[:alpha:]+"),
-         genus = 
-           str_extract(species_scientific_name, "[:alpha:]+$"),
-         .after = species_scientific_name) %>% 
+  
+  # recode common name "Shy / white-capped / Salvin's....." to "White capped albatross" 
+  mutate(species_common_name =
+           case_when(str_detect(species_common_name, "Shy /")
+                     ~ "White capped albatross",
+                     .default = species_common_name)) %>% 
+  # tidy names
+  mutate(
+    # remove the capital letters at the end of the common name for age
+    species_common_name = 
+      str_remove(species_common_name, "[:upper:]*[0-9]*$"),
+    
+    # remove the capital letters at the end of the abbreviation for age
+    species_abbreviation = 
+      str_extract(species_abbreviation, "^[:upper:]+"),
+    
+    # new column for just family
+    family =
+      str_extract(species_scientific_name, "^[:alpha:]+"),
+    
+    # new column for just genus
+    genus = 
+      str_extract(species_scientific_name, "[:alpha:]+$"),
+    .after = species_scientific_name) %>% 
+  
   relocate(family, .after = species_scientific_name)
 
+  
 # Write csv
 write_csv(seabirds_joined, "clean_data/seabirds_clean.csv")
