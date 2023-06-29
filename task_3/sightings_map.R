389d5485617ab54b94443e2fe5b3551b9afca1f9
Emmz900 <68696398+Emmz900@users.noreply.github.com>
2023-06-29 12:15:44 +0100

    experimented with shiny

--- task_3/sightings_map.R
+++ task_3/sightings_map.R
@@ -1,13 +1,17 @@
 library(shiny)
 library(leaflet)
 library(tidyverse)
+library(htmltools)
+library(htmlwidgets)
 
 seabirds <- read_csv("clean_data/seabirds_clean.csv")
 
 seabirds <- seabirds %>% 
-  mutate(species_common_name = str_remove(species_common_name, "\\(unidentified\\)"))
+  filter(!is.na(lat) & !is.na(long)) %>% 
+  mutate(species_common_name = str_to_title(str_remove(species_common_name, "\\(unidentified\\)|[A-Z]*$")),
+         type_of_bird = str_extract(species_common_name, "[A-Z][a-z]+$"), .after = species_common_name)
 
-species_options <- sort(unique(seabirds$species_common_name))
+bird_options <- sort(unique(seabirds$type_of_bird))
 
 ui <- fluidPage(
   
@@ -15,12 +19,15 @@ ui <- fluidPage(
   
   sidebarLayout(
     sidebarPanel(
-    selectInput("common_name", "Species Selection", species_options)
-  ),
-  
-  mainPanel(
-    leafletOutput("sightings_map")
-  )
+      selectInput("bird_input",
+                  "Species Selection",
+                  bird_options,
+                  selected = "Penguin")
+    ),
+    
+    mainPanel(
+      leafletOutput("sightings_map")
+    )
   )
   
   
@@ -28,6 +35,25 @@ ui <- fluidPage(
 )
 
 server <- function(input, output, session) {
+  
+  # boat <- makeIcon(
+  #   iconUrl = boat.file,
+  #   iconWidth = 10,
+  #   iconHeight = 25
+  # )
+  # 
+  # boat.file <- "http://cdn.onlinewebfonts.com/svg/download_498189.png"
+  
+  output$sightings_map <- renderLeaflet({
+    seabirds %>% 
+      filter(type_of_bird == input$bird_input) %>% 
+      leaflet() %>% 
+      addProviderTiles(provider = providers$OpenStreetMap) %>% 
+      addMarkers(lng = ~ long, lat = ~ lat,
+                        #icon = boat,
+                        #color = ~ species_common_name,
+                        label = ~species_common_name)
+  })
   
 }
 
